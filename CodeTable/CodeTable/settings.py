f2cecf50fd66f16469967bf816dcd2d6367b7e54
aman-iitj <ug201310004@iitj.ac.in>
2016-02-12 05:26:18 +0530

    Heroku 4 djdatabase

--- CodeTable/CodeTable/settings.py
+++ CodeTable/CodeTable/settings.py
@@ -14,7 +14,7 @@ import os
 from django.conf import settings
 import dj_database_url
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = { 'default': dj_database_url.config() }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
